5b82532da584a8a9cc3440ba34b08df188fe37f7
harpreet <Harpreet Kour>
2020-07-23 12:54:15 -0600

    added if else to fetch

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,18 @@
 fetch( "http://api.open-notify.org/astros.json" ) // Sends the request...
   // We can use .then() to parse the response.
   .then( response => {
-    console.log( response );
-  } )
+      if ( response.status >= 200 && response.status <=299 )
+      {//200 range status/ response codes are SUCCESSES.
+          return response.json() // convert to JSON , and send to next step (.then())
+      }
+      else {
+          //If it is another range, we are unsuccessful...
+          throw Error( response.statusText);// Display a formal error message reporting the concern.
+      }
+   return response.json()
+  })
+//   DATA has been formatted, lets have a look inside...
+  .then(data =>
+    {
+        console.log (data)
+    })
