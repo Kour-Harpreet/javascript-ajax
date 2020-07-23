0ab8d9e7c2b4fb2f58f65ab6e51d27aad0cb303c
harpreet <Harpreet Kour>
2020-07-23 13:55:55 -0600

    Install Axios AJAX/request library.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # javascript-ajax
+https://github.com/axios/axios
+http://api.open-notify.org/iss-now.json
+http://api.open-notify.org/astros.json
+https://www.w3schools.com/xml/ajax_info.txt
+https://github.com/TECHCareers-by-Manpower/4.1-javascript-ajax
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
 // Retrieving an external resource (live JSON data.)
-fetch( "http://api.open-notify.org/astros.json" ) // Sends the request...
-  // We can use .then() to parse the response.
-  .then( response => {
-      if ( response.status >= 200 && response.status <=299 )
-      {//200 range status/ response codes are SUCCESSES.
-          return response.json() // convert to JSON , and send to next step (.then())
-      }
-      else {
-          //If it is another range, we are unsuccessful...
-          throw Error( response.statusText);// Display a formal error message reporting the concern.
-      }
-   return response.json()
-  })
-//   DATA has been formatted, lets have a look inside...
-  .then(data =>
-    {
-        console.log (data)
+fetch("http://api.open-notify.org/astros.json") // Sends the request...
+    // We can use .then() to parse the response.
+    .then(response => {
+        if (response.status>=200&&response.status<=299) {//200 range status/ response codes are SUCCESSES.
+            return response.json() // convert to JSON , and send to next step (.then())
+        }
+        else {
+            //If it is another range, we are unsuccessful...
+            throw Error(response.statusText);// Display a formal error message reporting the concern.
+        }
+        return response.json()
     })
+    //   DATA has been formatted, lets have a look inside...
+    .then(data => {
+        console.log(data)
+    // Prepare for HTML list.
+    const peopleUL = document.createElement( "UL" );
+    // Let's grab those people!
+    const people = data.people; // This is an array of people...
+    // Let's loop through them!
+    for ( const person of people )
+    { // Prepare an LI for this person.
+      const personLI = document.createElement( "LI" );
+      // Add some text content (using template literal to inject our values.)
+      personLI.textContent = `${person.name} is currently aboard ${person.craft}.`;
+      // Add this <li> to our <ul>.
+      peopleUL.appendChild( personLI );
+    }
+    // Add our <ul> to the <body> so we can see it in the browser!
+    document.body.appendChild( peopleUL );
+  } );
+  axios; // If we don't get an error on this line, Axios is installed and working!
